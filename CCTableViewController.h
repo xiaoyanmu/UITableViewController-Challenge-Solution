3113304796a83a4b54d75a7f031e4e90d88acadd
xiaoyan wang <redfox21st2000@yahoo.com>
2014-05-14 13:36:36 -0500

    sloved the challenge

--- CCTableViewController.h
+++ CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  CCViewController.h
+//  CCTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by xiaoyan wang on 5/14/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface CCTableViewController : UITableViewController
 
 @end
